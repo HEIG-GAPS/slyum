4d7730dce6c0cf2a7942ebd6a1c1fc769340a461
david.miserez <david.miserez@gmail.com>
2011-12-01 15:08:20 +0100

    add showMessage with Component argument

--- src/utility/SMessageDialog.java
+++ src/utility/SMessageDialog.java
@@ -1,5 +1,7 @@
 package utility;
 
+import java.awt.Component;
+
 import javax.swing.JOptionPane;
 
 import swing.Slyum;
@@ -10,31 +12,61 @@ public class SMessageDialog
 	
 	public static void showErrorMessage(String message)
 	{
-		JOptionPane.showMessageDialog(Slyum.getInstance(), message, TITLE_WINDOW, JOptionPane.ERROR_MESSAGE);
+		showErrorMessage(message, Slyum.getInstance());
+	}
+	
+	public static void showErrorMessage(String message, Component c)
+	{
+		JOptionPane.showMessageDialog(c, message, TITLE_WINDOW, JOptionPane.ERROR_MESSAGE);
 	}
 	
 	public static void showWarningMessage(String message)
 	{
-		JOptionPane.showMessageDialog(Slyum.getInstance(), message, TITLE_WINDOW, JOptionPane.WARNING_MESSAGE);
+		showWarningMessage(message, Slyum.getInstance());
+	}
+	
+	public static void showWarningMessage(String message, Component c)
+	{
+		JOptionPane.showMessageDialog(c, message, TITLE_WINDOW, JOptionPane.WARNING_MESSAGE);
 	}
 	
 	public static void showInformationMessage(String message)
 	{
-		JOptionPane.showMessageDialog(Slyum.getInstance(), message, TITLE_WINDOW, JOptionPane.INFORMATION_MESSAGE);
+		showInformationMessage(message, Slyum.getInstance());
+	}
+	
+	public static void showInformationMessage(String message, Component c)
+	{
+		JOptionPane.showMessageDialog(c, message, TITLE_WINDOW, JOptionPane.INFORMATION_MESSAGE);
 	}
 	
 	public static int showQuestionMessageYesNo(String message)
 	{
-		return JOptionPane.showConfirmDialog(Slyum.getInstance(), message, TITLE_WINDOW, JOptionPane.YES_NO_OPTION);
+		return showQuestionMessageYesNo(message, Slyum.getInstance());
+	}
+	
+	public static int showQuestionMessageYesNo(String message, Component c)
+	{
+		return JOptionPane.showConfirmDialog(c, message, TITLE_WINDOW, JOptionPane.YES_NO_OPTION);
 	}
 	
 	public static int showQuestionMessageOkCancel(String message)
 	{
-		return JOptionPane.showConfirmDialog(Slyum.getInstance(), message, TITLE_WINDOW, JOptionPane.OK_CANCEL_OPTION);
+		return showQuestionMessageOkCancel(message, Slyum.getInstance());
+	}
+	
+	public static int showQuestionMessageOkCancel(String message, Component c)
+	{
+		return JOptionPane.showConfirmDialog(c, message, TITLE_WINDOW, JOptionPane.OK_CANCEL_OPTION);
 	}
 	
 	public static int showQuestionMessageYesNoCancel(String message)
 	{
-		return JOptionPane.showConfirmDialog(Slyum.getInstance(), message, TITLE_WINDOW, JOptionPane.YES_NO_CANCEL_OPTION);
+		return showQuestionMessageYesNoCancel(message, Slyum.getInstance());
+	}
+	
+	public static int showQuestionMessageYesNoCancel(String message, Component c)
+	{
+		return JOptionPane.showConfirmDialog(c, message, TITLE_WINDOW, JOptionPane.YES_NO_CANCEL_OPTION);
 	}
 }
