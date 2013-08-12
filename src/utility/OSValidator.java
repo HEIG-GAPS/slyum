61ea369b357e789a00d00e294beb69ac7b1afd6a
miserezd <miserezd@localhost>
2013-08-12 09:51:51 +0200

    fix bug detect unix os

--- src/utility/OSValidator.java
+++ src/utility/OSValidator.java
@@ -2,35 +2,30 @@ package utility;
 
 /**
  * Detect wich os is installed on the system running Java.
- * 
- * @author
- *         http://www.mkyong.com/java/how-to-detect-os-in-java-systemgetpropertyosname
- *         /
- * 
  */
-public class OSValidator
-{
-	public static boolean isMac()
-	{
+public class OSValidator {
+  
+  public static boolean IS_MAC = isMac();
+  public static boolean IS_UNIX = isUnix();
+  public static boolean IS_WINDOWS = isWindows();  
+  
+	private static boolean isMac() {
 		final String os = System.getProperty("os.name").toLowerCase();
 		// Mac
 		return os.indexOf("mac") >= 0;
 
 	}
 
-	public static boolean isUnix()
-	{
+	private static boolean isUnix() {
 		final String os = System.getProperty("os.name").toLowerCase();
 		// linux or unix
 		return os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0;
 
 	}
 
-	public static boolean isWindows()
-	{
+	private static boolean isWindows() {
 		final String os = System.getProperty("os.name").toLowerCase();
 		// windows
 		return os.indexOf("win") >= 0;
-
 	}
 }
