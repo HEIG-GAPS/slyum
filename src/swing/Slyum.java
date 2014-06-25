6c9866e835c88c208b3938c57f703f885e1c70d4
miserezd <miserezd@localhost>
2014-06-25 13:06:31 +0200

    remove sintax checking

--- src/swing/Slyum.java
+++ src/swing/Slyum.java
@@ -58,7 +58,7 @@ import utility.SMessageDialog;
 public class Slyum extends JFrame implements ActionListener {
   
   private static final String APP_NAME = "Slyum";
-  public static final String VERSION = "4.0.3";
+  public static final String VERSION = "4.1.0";
   public final static String EXTENTION = "sly";
   public final static String FULL_EXTENTION = String.format(".%s", EXTENTION);
   public final static String APP_DIR_NAME = APP_NAME;
